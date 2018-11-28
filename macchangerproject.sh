859dc90e9a7d9010ce1a2c54cacfc83f03adb441
John Fawcett <43934323+jwfawcett@users.noreply.github.com>
2018-11-27 20:23:45 -0500

    Add files via upload

---
+++ macchangerproject.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+clear
+
+echo "What would you like your new mac to be? exp. 00:11:22:33:44:55"
+
+read newmac
+# creates the mac to be changed to
+
+echo
+#new line
+echo "Cool let us change that mac"
+echo
+
+ifconfig wlan0 down
+macchanger --mac $newmac wlan0
+ifconfig wlan0 up
+#changes mac
+
+echo"Your new mac address is"
+ifconfig | grep ether
+#gathers new mac 
+#by John Fawcett
