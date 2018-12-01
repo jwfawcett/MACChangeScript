82849be867a357a66c8a3cacb16fe472fbf70612
John Fawcett <43934323+jwfawcett@users.noreply.github.com>
2018-11-30 19:03:07 -0500

    Update macchangerproject.sh

--- macchangerproject.sh
+++ macchangerproject.sh
@@ -20,4 +20,8 @@ echo
 echo "Your new mac address is"
 ifconfig | grep ether
 #gathers new mac 
+
+#New Project To add. I want to then automate:
+#aireplay-ng -0 1 -a 00:11:22:33:44:55 -c 00:11:22:33:44:55 ath0
+#after changing mac deauth someone else from the network
 #by John Fawcett
